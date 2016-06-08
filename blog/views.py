8ba102386f8ade66bbf051b6d9522239640cd919
ntntktk <afamiliar22@gmail.com>
2016-06-08 15:27:16 +0900

    category_new 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Shop, Review
 
 def index(request):
     shop_list = Category.objects.all()
     return render(request, 'blog/index.html', {
         'shop_list': shop_list,
+        })
+
+def category_new(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    return render(request, 'blog/category.html', {
+        'category': category,
         })
