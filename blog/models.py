ed0383107beb66714aa77cbb91604bfb6f936f0e
ntntktk <afamiliar22@gmail.com>
2016-06-08 14:58:54 +0900

    Category, Shop, Review 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Shop(models.Model):
+    title = models.CharField(max_length=100)
+    content_phone = models.CharField(max_length=100)
+    content_adress = models.CharField(max_length=100)
+    content_detail = models.TextField()
+    photo = models.ImageField()
+
+class Review(models.Model):
+    post = models.ForeignKey(Shop)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
