47f4b078e6eedb5db12870c6550a78783d1bfc00
ntntktk <afamiliar22@gmail.com>
2016-06-08 16:23:54 +0900

    댓글 수정 구현

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Comment
+from .models import Review
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment
+        model = Review
         fields = ['message']
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<pk>\d+)/$', views.category_detail, name='category_detail'),
+    url(r'^(?P<category_pk>\d+)/comments/new$', views.review_new, name='review_new'),
+    url(r'^(?P<category_pk>\d+)/comments/(?P<pk>\d+)/edit/$', views.review_edit, name='review_edit'),
 ]
