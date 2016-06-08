e5bc353a75e9f375196c2697d62d3cabd4e5275a
ntntktk <afamiliar22@gmail.com>
2016-06-08 15:46:09 +0900

    category_new, edit, detail 뷰 생성

--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,34 @@ def index(request):
         'shop_list': shop_list,
         })
 
-def category_new(request, pk):
+def category_detail(request, pk):
     category = get_object_or_404(Category, pk=pk)
     return render(request, 'blog/category.html', {
         'category': category,
         })
+
+def category_new(request, category_pk):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = get_object_or_404(Category, pk=category_pk)
+            comment.user = request.user
+            comment.save()
+            messages.success(request, '새로운 카테고리가 생성되었습니다.')
+            return redirect('blog:category_detail', category_pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/category.html')
+
+    def category_edit(request, category_pk, pk):
+        comment = get_object_or_404(Category, pk=pk)
+
+        if request.method == 'POST':
+            form = CommentForm(request,POST, instance=comment)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = get_object_or_404(Category, pk=category_pk)
+                comment.user = request.user
+                comment.save()
+                messages.success(request, '새로운 카테고리가 생성되었습니다.')
